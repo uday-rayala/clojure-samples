eb28063ca3555972cc6a7fefabc7209bda3540d5
Casper <casper@springer.com>
2011-08-02 18:51:28 +0100

    Changed extension

---
+++ js/fancy_go_radiator.user.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name           Fancy Go! Radiator
+// @namespace      http://www.thoughtworks.com/radiator
+// @description    Fancy Go! Radiator
+// @include        http://172.18.10.196:8153/go/pipelines
+// ==/UserScript==
+
+var $;
+
+// Add jQuery
+    (function(){
+        if (typeof unsafeWindow.jQuery == 'undefined') {
+            var GM_Head = document.getElementsByTagName('head')[0] || document.documentElement,
+                GM_JQ = document.createElement('script');
+    
+            GM_JQ.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';
+            GM_JQ.type = 'text/javascript';
+            GM_JQ.async = true;
+    
+            GM_Head.insertBefore(GM_JQ, GM_Head.firstChild);
+        }
+        GM_wait();
+    })();
+
+// Check if jQuery's loaded
+    function GM_wait() {
+        if (typeof unsafeWindow.jQuery == 'undefined') {
+            window.setTimeout(GM_wait, 100);
+        } else {
+            $ = unsafeWindow.jQuery.noConflict(true);
+            letsJQuery();
+        }
+    }
+
+// All your GM code must be inside this function
+    function letsJQuery() {
+      $('#header').remove()
+      $('.page_header').remove()
+      $('#footer').remove()
+    }
