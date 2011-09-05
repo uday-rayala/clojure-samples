66897dae27e91f71518cb39db21b150095202793
Udayakumar Rayala <uday.rayala@gmail.com>
2011-09-05 23:02:05 +0100

    Added slider to control the commit sizes

---
+++ GitStats/resources/public/code-changes.js
@@ -0,0 +1,50 @@
+var dataURL = "/code-changes-plain.json";
+var jsonData = $.ajax({ type: "GET", url: dataURL, async: false }).responseText;
+var commits = JSON.parse(jsonData);
+
+var startDate = new Date(commits[0].date);
+var endDate = new Date(commits[commits.length - 1].date);
+var numberOfDays = Math.ceil((endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24))
+
+var goodDay = new Date(1990, 1, 1, 5, 0);
+var nextGoodDay = new Date(1990, 1, 1, 23, 0);
+
+var data = $.map(commits, function(commit) {
+    var commitDate = new Date(commit.date);
+    var minutes = new Date(goodDay);
+    minutes.setHours(commitDate.getHours());
+    minutes.setMinutes(commitDate.getMinutes());
+
+    return {x: commitDate, y: minutes, z: commit.size, m: commit.message};
+});
+
+data.sort(function(a, b) {
+    return b.z - a.z;
+});
+
+var sizes = $.map(data, function (d) {
+    return d.z;
+});
+
+var minValue = Math.min.apply( Math, sizes);
+var maxValue = Math.max.apply( Math, sizes);
+
+var dateFormat = pv.Format.date("%d/%B");
+var timeFormat = pv.Format.date("%H:%M");
+
+$(document).ready(function() {
+    $( "#slider-range" ).slider({
+        range: true,
+        min: minValue,
+        max: maxValue,
+        values: [ minValue, maxValue ],
+        slide: function( event, ui ) {
+            minValue = ui.values[ 0 ];
+            maxValue = ui.values[ 1 ];
+            $( "#sizes" ).val(ui.values[ 0 ] + " - " + ui.values[ 1 ] );
+        },
+        stop: function( event, ui ) {
+            vis.render();
+        }
+    });
+});
