55901c5c6c05627976f7f170a6369a63562310e1
Mark Needham <m.h.needham@gmail.com>
2011-09-06 20:07:55 +0100

    mark: story graph

---
+++ GitStats/resources/public/changes-by-story.js
@@ -0,0 +1,78 @@
+var chart;
+var areas,names;
+var codeData;
+
+function getData(){
+	var dataUrl = "/stories-and-commiters.json"
+    var jsonData = $.ajax({type : 'GET', url:dataUrl, async:false}).responseText;
+    stories = JSON.parse(jsonData);	
+}
+
+function byStory() {
+   var values = [];
+   $.each(stories, function(index, story) {
+	 var value = { story:story.story, commiters: [] };
+     var numberOfCommiters = story.commiters.length;
+     $.each(story.commiters, function(index, commiter) {
+	   value.commiters.push([commiter, 100 / numberOfCommiters]);
+     })	;
+     values.push(value);
+   });
+   return values;
+}
+
+
+function drawChart(container, id, seriesValues) {
+
+   var options = {
+	   		      chart: { renderTo: container},
+	   		      title: {
+	   		         text: 'Story #' + id
+	   		      },
+	   		      tooltip: {
+	   		         formatter: function() {
+	   		            return '<b>'+ this.point.name +'</b>: '+ this.percentage.toFixed(2) +' %';
+	   		         }
+	   		      },
+	   		      plotOptions: {
+	   		         pie: {
+	   		            allowPointSelect: true,
+	   		            cursor: 'pointer',
+	   		            dataLabels: {
+	   		               enabled: true,
+	   		               color: Highcharts.theme.textColor || '#000000',
+	   		               connectorColor: Highcharts.theme.textColor || '#000000',
+	   		               formatter: function() {
+	   		                  return '<b>'+ this.point.name +'</b>: '+ this.percentage.toFixed(2) +' %';
+	   		               }
+	   		            }
+	   		         }
+	   		      },
+	   		       series: [{
+	   		         type: 'pie',
+	   		         name: 'Story Commiters',
+	   		         data: seriesValues
+	   		      }]
+	   		   }
+
+   new Highcharts.Chart(options);
+
+}
+
+$(document).ready(function() {
+    getData();
+    var i = 0;
+
+    $.each(byStory(), function(index, story) {
+        var containerId = 'container'+i;
+        var container = $('<div/>');
+        container.attr('id', containerId);
+        container.attr('class', 'container');
+        $('#by-story').append(container);
+        i++;
+
+        console.log(story);
+
+        drawChart(containerId, story.story, story.commiters);
+    });
+});
