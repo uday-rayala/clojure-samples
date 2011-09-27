8d4c47e11f8a7b76cf67a2f336470ac52a0fe553
Udayakumar Rayala <uday.rayala@gmail.com>
2011-09-27 17:56:43 +0100

    Uday: Adding identity and track

--- GitStats/generate-logs.sh
+++ GitStats/generate-logs.sh
@@ -13,19 +13,34 @@ cd $REPOS_DIR/core
 git pull --rebase
 
 git log --no-merges --ignore-all-space --pretty="format:%s" --after="$TODAY" >> $TMP_LOGS_DIR/today-commits.txt
+
 git log --no-merges --ignore-all-space --pretty="format:%cd | %s" > $TMP_LOGS_DIR/core-commits.txt
 git log -l30000 --no-merges --ignore-all-space --shortstat --ignore-all-space --pretty="format:%cd%n%s" --find-copies > $TMP_LOGS_DIR/core-code-change-commits.txt
 git log -l30000 --no-merges --ignore-all-space --pretty="format:%s" --numstat --find-copies > $TMP_LOGS_DIR/core-message-and-changes.txt
 
-echo "Getting core logs from $REPOS_DIR/aim"
+echo "Getting aim logs from $REPOS_DIR/aim"
 cd $REPOS_DIR/aim
 git pull --rebase
 
 echo >> $TMP_LOGS_DIR/today-commits.txt
 git log --no-merges --ignore-all-space --pretty="format:%s" --after="$TODAY" >> $TMP_LOGS_DIR/today-commits.txt
+
 git log --no-merges --ignore-all-space --pretty="format:%cd | %s" > $TMP_LOGS_DIR/aim-commits.txt
 git log -l30000 --no-merges --ignore-all-space --shortstat --ignore-all-space --pretty="format:%cd%n%s" --find-copies > $TMP_LOGS_DIR/aim-code-change-commits.txt
 
+echo "Getting identity logs from $REPOS_DIR/identity"
+cd $REPOS_DIR/identity
+git pull --rebase
+
+echo >> $TMP_LOGS_DIR/today-commits.txt
+git log --no-merges --ignore-all-space --pretty="format:%s" --after="$TODAY" >> $TMP_LOGS_DIR/today-commits.txt
+
+echo "Getting track logs from $REPOS_DIR/track"
+cd $REPOS_DIR/track
+git pull --rebase
+
+echo >> $TMP_LOGS_DIR/today-commits.txt
+git log --no-merges --ignore-all-space --pretty="format:%s" --after="$TODAY" >> $TMP_LOGS_DIR/today-commits.txt
 
 popd
 
