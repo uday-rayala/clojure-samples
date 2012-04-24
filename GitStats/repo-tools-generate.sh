886bbeaa9a5bd5ce85e1f7c177a01b32d1334789
Udayakumar Rayala <uday.rayala@gmail.com>
2012-04-24 19:00:21 +0100

    Uday New repos dir..for some reason the old dir is not working

--- GitStats/repo-tools-generate.sh
+++ GitStats/repo-tools-generate.sh
@@ -1,5 +1,5 @@
 export GIT_LOGS_DIR=/home/casper/graphs/clojure-samples/GitStats/logs
-export GIT_REPOS_DIR=/home/casper/graphs/repos
+export GIT_REPOS_DIR=/home/casper/graphs/new-repos
 export TMP_LOGS_DIR=/home/casper/graphs/tmplogs
 
 
